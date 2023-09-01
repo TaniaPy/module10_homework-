6b88e145e8069e3bfe04379101814d6093d4e695
TaniaPy <135258589+TaniaPy@users.noreply.github.com>
2023-09-01 20:30:21 +0500

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,135 @@
+//var a = 1;
+//const b = 2;
+//let sum = a + b;
+//console.log(a, b, sum)
+ //var a = true;
+//var b = false;
+//console.log(a && b)
+ 
+
+//Задание 1
+//var number =+ prompt("Введите четное число");
+//console.log(number);
+
+//if (typeof number === 'number' && !isNaN(number)) {
+   // if (number % 2 === 0) {
+       // console.log("Число четное");
+   // } else {
+     //   console.log("число нечетное");
+   // }
+//} else {
+  //  console.log("Упс, кажется, вы ошиблись");
+//}
+//Задание 1
+
+
+//Задание 2
+//const x = 55;
+//switch (typeof(x)) {
+ // case 'string':
+   // console.log('x - строка');
+  //  break;
+ // case 'boolean':
+   // console.log('x - логический тип');
+ //   break;
+ // case 'number':
+ //  console.log('x - число');
+   // break;
+  //default:
+   // console.log('Тип x не определен');
+//}
+//Задание 2
+
+//Задание 3
+//let reverse = a => a.split('') 
+                   // .reverse() 
+                    //.join(''); 
+
+//console.log(reverse('hello'))
+
+//Задание 3
+
+
+//Задание 4
+//let mathDictates = 55;
+//let mathDictates1 = 91;
+
+//console.log(mathDictates / mathDictates1)
+
+//Задание 4
+
+
+
+//Задание 5
+//let arr = ['3', '6', '12', '55', '60', '31', '10'];
+//console.log(arr.length);
+
+//for (let i = 0; i < arr.length; i++){
+    //console.log(arr[i]);
+//}
+
+//Задание 5
+
+//Задание 6
+//let arr = ['3', '6', '12', '55', '60', '31', '10'];
+
+//let another = true;
+//for (let i = 0; i < arr.length - 1; i++){
+   // for (let a = i + 1; a < arr.length; a++){
+        //if (typeof(arr[i]) !== typeof(arr[a])){
+         //   another = false;
+       // }
+   // }
+//}
+//if (another) {
+  //  console.log("array another");
+//} else {
+ //   console.log("array not another");
+//}
+//Задание 6
+
+
+//Задание 7
+//let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'a', null];
+//let even = 0; 
+//let odd = 0; 
+//let another = 0; 
+
+//for (let i = 0; i < arr.length; i++){
+   // if ((arr[i] === 0) || (typeof(arr[i]) != 'number')) {
+    //    another++;
+    //} else {
+     //   if (arr[i] % 2 === 0) {
+     //       even++;
+      //  } else {
+      //      odd++;
+      //  }
+  //  }
+//}
+
+// console.log("Even is " + even);
+// console.log("Odd is " + odd);
+// console.log("Another is " + another);
+
+//Задание 7
+
+
+
+
+//Задание 8
+// let fruits = new Map();
+// fruits.set("orange", "orange");
+// fruits.set("Banana", "yellow");
+// fruits.set("nod", "green");
+// fruits.set("cherry", "burgundy");
+// fruits.set("nectarine", "red");
+// fruits.set("grapes", "purpel");
+
+
+// for (let user of fruits.keys()){
+//     console.log(user + " is " + fruits.get(user));
+// }
+//Задание 8
+
+
+
